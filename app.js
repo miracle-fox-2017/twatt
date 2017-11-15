bf5cdcd9e71f3c26a8baf80ab6d7395195937b7d
aditReza <lete.note3@gmail.com>
2017-11-15 11:52:06 +0700

    release 0 | lets start the project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//Routes
+const twitter = require('./routes/twitter')
+app.use('/', twitter)
+
+app.listen(3000, function(err){
+  if(!err) console.log('server running on port|3000')
+})
---
+++ controllers/twitter.js
@@ -0,0 +1,8 @@
+const OAuth = require('oauth')
+require('dotenv').config()
+const secret = process.env.secretKey
+
+
+module.exports = {
+  
+}
