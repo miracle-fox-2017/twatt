9a65620029cf662786cb018d7dfc7d81d726d19c
AhmadNizar <ahmadnizar.owl@gmail.com>
2017-11-16 08:22:43 +0700

    -add client -add refresh button

--- server/app.js
+++ server/app.js
@@ -9,6 +9,12 @@ app.use(bodyParser.json())
 
 const twatt = require('./routes/twattRoute')
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/api/twatt', twatt)
 
 app.listen(3000, () => {
