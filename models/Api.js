1927b276b5f8002a0e5575870e732794b7bf9e1e
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-11-20 07:44:28 +0700

    rapi-rapi season 2

--- models/Api.js
+++
@@ -1,9 +0,0 @@
-const OAuth = require('oauth').OAuth;
-require('dotenv').config()
-
-module.exports = new OAuth(
-  "http://twitter.com/oauth/request_token",
-  "http://twitter.com/oauth/access_token",
-  process.env.CONSUMER_KEY, process.env.CONSUMER_SECRET,
-  "1.0A", null, "HMAC-SHA1"
-);
--- routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const apiesController = require('../controllers/api')
-
-/* GET apies listing. */
-router.get('/search', apiesController.searchTweet)
-router.get('/timeline', apiesController.timelineTweet)
-
-module.exports = router;
